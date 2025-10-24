f2f85478afd7bc0d4a3141c804df3bca6c1fc004
KingElmagli <elmaglimostafa@gmail.com>
2025-10-24 17:56:45 +0100

    activity 7
    added icons and modify and delete labels

--- src/components/entity/modules/ModuleView.js
+++ src/components/entity/modules/ModuleView.js
@@ -1,6 +1,7 @@
 import { StyleSheet, Text, View } from "react-native";
 import FullWidthImage from "react-native-fullwidth-image";
-
+import { Button, ButtonTray } from "../../UI/Button";
+import Icons from "../../UI/Icons";
 const ModuleView = ({ module }) => {
   //Initialistaion
   //State
@@ -24,6 +25,15 @@ const ModuleView = ({ module }) => {
           <Text style={styles.dimText}> (Module Leader) </Text>
         </Text>
       </View>
+      <ButtonTray>
+        <Button icon={<Icons.edit />} label="Modify" />
+        <Button
+          icon={<Icons.Delete />}
+          label="Delete"
+          styleButton={{ backgroundColor: "mistyrose" }}
+          styleLabel={{ color: "red" }}
+        />
+      </ButtonTray>
     </View>
   );
 };
